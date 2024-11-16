b02bccc38496cd81bbcead5c59833e3f414d6c67
edrikfsteiner <edrikfsteiner@gmail.com>
2024-11-16 08:49:01 -0300

    Repository

---
+++ src/main/java/com/hospital/repository/AtendimentoRepository.java
@@ -0,0 +1,8 @@
+package com.hospital.repository;
+
+import com.hospital.model.Atendimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AtendimentoRepository extends JpaRepository<Atendimento, Long> {}
---
+++ src/main/java/com/hospital/repository/MedicamentoRepository.java
@@ -0,0 +1,8 @@
+package com.hospital.repository;
+
+import com.hospital.model.Medicamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {}
